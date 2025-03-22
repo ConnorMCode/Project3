9ee6425ce30c2789beeaa70964996cc92f48ded1
ConnorMCode <98790570+ConnorMCode@users.noreply.github.com>
2025-03-22 09:05:16 -0700

    Catch syscall.h up to proj 2 solution

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -2,7 +2,10 @@
 #define USERPROG_SYSCALL_H
 
 #include <stdbool.h>
+#include "threads/thread.h"
 
 void syscall_init (void);
+void exit (int status);
+void clear_all_files(struct thread *cur);
 
 #endif /* userprog/syscall.h */
